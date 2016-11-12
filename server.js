926e566db8175fcb54039ae7122b91b8c6a18d66
ryrizo <perrizoryan@gmail.com>
2016-11-12 00:50:44 -0500

    Update for heroku port and start script

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -18,12 +20,12 @@ app.use((req, res, next) => { //req gives me access to data about the request in
   next();
 });
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs', {
-    pageTitle: 'Maintenance Mode',
-    welcomeMessage: 'Be back shortly'
-  });
-});
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance Mode',
+//     welcomeMessage: 'Be back shortly'
+//   });
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -55,6 +57,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
