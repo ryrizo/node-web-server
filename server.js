5eea967824c38417410c9869d3c24c4ae92e49ee
ryrizo <perrizoryan@gmail.com>
2016-11-12 00:13:17 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => { //req gives me access to data about the request in progress
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) { // callback not required in this function but it is in node v7
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance Mode',
+    welcomeMessage: 'Be back shortly'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    welcomeMessage: 'Welcome to my page. Ticallllll',
+    pageTitle: 'Welcome Page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Error handling request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
